f186f9342c7a01bb1e7aa05feb33720d23ead08b
Hakim <125070824+anynonenom@users.noreply.github.com>
2024-08-24 22:39:57 +0100

    Create Memorize.hpp

---
+++ Memorizer_game/Memorize.hpp
@@ -0,0 +1,31 @@
+//
+//  Memorize.hpp
+//  coding
+//
+//  Created by mac on 14/8/2024.
+//
+
+#ifndef Memorize_hpp
+#define Memorize_hpp
+#define N_X 26
+#include<iostream>                                                          		
+#include <stdio.h>
+#include<vector>
+#include<random>
+#include<algorithm>
+using namespace std ;
+class Memorize {
+private:
+    
+    vector<char> vect ;
+    //stack<char> stck ;
+    int i = 0 ;
+public:
+    
+    ~Memorize() ;
+    Memorize(char );
+    void inner_random( ) ;
+    void view_output() ;
+    void Hit_Box() ;
+};
+#endif /* Memorize_hpp */
